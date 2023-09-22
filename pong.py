18161c3dab55d0faea9672ec16b5f79586456b77
Kim Readman <135356483+Kim-Read@users.noreply.github.com>
2023-09-22 13:23:46 +0100

    Update pong.py
    Changed screen.
    Added functions.
    Added keystrokes.

--- pong.py
+++ pong.py
@@ -1,21 +1,23 @@
 from turtle import *
 import time
 
-# Set screen size
-setup(400, 300)
-
-# Set background colour - change this to something pretty
-bgcolor("black")
+screen = Screen()
+screen.title("Pong game")
+screen.setup(width=1000 , height=600)
 
 # Create a ball - change this to something interesting - ball = pie
 ball = Turtle()
 ball.shape("circle")
 ball.color("green")
+ball.speed(0)
 ball.penup()
 ball.goto(0,0)
+ball.dx = 2
+ball.dy = -2
 
 # Create paddle 1
 paddle1 = Turtle()
+paddle1.speed(0)
 paddle1.shape("square")
 paddle1.color("blue")
 paddle1.shapesize(stretch_wid = 5, stretch_len = 1)
@@ -25,6 +27,7 @@ paddle1.dy = 0
 
 # Create paddle 2
 paddle2 = Turtle()
+paddle2.speed(0)
 paddle2.shape("square")
 paddle2.color("purple")
 paddle2.shapesize(stretch_wid = 5, stretch_len = 1)
@@ -32,6 +35,30 @@ paddle2.penup()
 paddle2.goto(350, 0)
 paddle2.dy = 0
 
+# Game Rules
+player1 = 0
+player2 = 0
+
+score = Turtle()
+score.speed(0)
+score.penup()
+#Hiding the turtle to show text
+score.hideturtle()
+#Locating the scoreboard on top of the screen
+score.goto(0, 260)
+#Showing the score
+score.write("Player1 : 0 Player2: 0", align="center", font=("Courier", 20, "bold"))
+
+#def move_pad1_up():
+
+#def move_pad1_down():
+
+#def move_pad2_down():
+
+#def move_pad2_up():
+
+#add keystrokes
 
 
-time.sleep(10)
+
+mainloop()
